4276f713f3cbfb4ffd0e1cb27ca627f3a503cf10
Dominick <dominick.odujebe@ucdconnect.ie>
2026-04-22 22:59:05 +0100

    Added more comprehensive bot testing

--- src/test/java/BotControllerTest.java
+++ src/test/java/BotControllerTest.java
@@ -47,4 +47,145 @@ public class BotControllerTest {
 
         assertFalse(botController.getLastScores().isEmpty());
     }
+
+    @Test
+    void test_SR8_openingStrategyIsUsedOnFirstMove() {
+        QuaxBoard board = new QuaxBoard((Theme) null);
+        BotController bot = new BotController();
+
+        bot.makeMove(board);
+
+        assertEquals("Opening Strategy", bot.getLastStrategyUsed());
+    }
+
+    @Test
+    void test_SR8_openingMoveIsCentral() {
+        QuaxBoard board = new QuaxBoard((Theme) null);
+        BotController bot = new BotController();
+
+        bot.makeMove(board);
+        Move move = bot.getLastMove();
+
+        boolean isCentral =
+                (move.getX() == 5 && move.getY() == 5) ||
+                        (move.getX() == 5 && move.getY() == 4) ||
+                        (move.getX() == 5 && move.getY() == 6) ||
+                        (move.getX() == 4 && move.getY() == 5) ||
+                        (move.getX() == 6 && move.getY() == 5);
+
+        assertTrue(isCentral);
+    }
+
+    @Test
+    void test_SR8_pathfindingStrategyUsedAfterOpening() {
+        QuaxBoard board = new QuaxBoard((Theme) null);
+        BotController bot = new BotController();
+
+        bot.makeMove(board);
+        bot.makeMove(board);
+
+        assertEquals("Pathfinding Strategy", bot.getLastStrategyUsed());
+    }
+
+    @Test
+    void test_SR8_strategyExplanationIsSet() {
+        QuaxBoard board = new QuaxBoard((Theme) null);
+        BotController bot = new BotController();
+
+        bot.makeMove(board);
+
+        assertNotEquals("", bot.getLastExplanation());
+    }
+
+    @Test
+    void test_SR8_moveIsAppliedToBoard() {
+        QuaxBoard board = new QuaxBoard((Theme) null);
+        BotController bot = new BotController();
+
+        bot.makeMove(board);
+        Move move = bot.getLastMove();
+
+        if (move.isRhombus()) {
+            assertNotEquals(Colour.NULL, board.getRhombus(move.getX(), move.getY()));
+        } else {
+            assertNotEquals(Colour.NULL, board.getStone(move.getX(), move.getY()));
+        }
+    }
+
+    @Test
+    void test_SR8_moveIsWithinBounds() {
+        QuaxBoard board = new QuaxBoard((Theme) null);
+        BotController bot = new BotController();
+
+        bot.makeMove(board);
+        Move move = bot.getLastMove();
+
+        if (move.isRhombus()) {
+            assertTrue(board.inBoundsRhombus(move.getX(), move.getY()));
+        } else {
+            assertTrue(board.inBoundsStone(move.getX(), move.getY()));
+        }
+    }
+
+    @Test
+    void test_SR8_botChoosesWinningMove() {
+        QuaxBoard board = new QuaxBoard((Theme) null);
+        BotController bot = new BotController();
+
+        // Builds vertical black chain
+        for (int y = 0; y < 10; y++) {
+            QuaxGame.placeStone(board, 5, y);
+            QuaxGame.placeStone(board, 0, y);
+        }
+
+        //Makes sure bot is Black and chooses the winning move
+        while (board.getTurn() != Turn.Player1) {
+            board.switchTurn();
+        }
+
+        Move move = bot.chooseMove(board);
+
+        assertNotNull(move);
+
+        QuaxBoard copy = new QuaxBoard(board);
+        QuaxGame.placeStone(copy, move.getX(), move.getY());
+
+        boolean wins = QuaxGame.checkWin(copy, move.getX(), move.getY());
+        assertTrue(wins);
+    }
+
+    @Test
+    void test_SR8_botBlocksWinningMove() {
+        QuaxBoard board = new QuaxBoard((Theme) null);
+        BotController bot = new BotController();
+
+        // Build white chain
+        for (int x = 0; x < 10; x++) {
+
+            while (board.getTurn() != Turn.Player2) {
+                board.switchTurn();
+            }
+            QuaxGame.placeStone(board, x, 5);
+
+            while (board.getTurn() != Turn.Player1) {
+                board.switchTurn();
+            }
+            QuaxGame.placeStone(board, x, 0);
+        }
+
+        //force bot to be BLACK
+        while (board.getTurn() != Turn.Player1) {
+            board.switchTurn();
+        }
+
+        // Bot should block at (10,5)
+        Move move = bot.chooseMove(board);
+
+        assertNotNull(move);
+        assertEquals(10, move.getX());
+        assertEquals(5, move.getY());
+    }
+
+
+
 }
